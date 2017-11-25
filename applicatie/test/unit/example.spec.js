56054742541aea445141754a68fcab452ee03ea2
Vince van Noort <vince.vannoort@student.hu.nl>
2017-11-25 19:45:59 +0100

    Check if test fails the build

--- applicatie/test/unit/example.spec.js
+++ applicatie/test/unit/example.spec.js
@@ -2,6 +2,6 @@
 
 const { test } = use('Test/Suite')('Example')
 
-test('make sure 2 + 2 is 4', async ({ assert }) => {
-  assert.equal(2 + 2, 4)
+test('make sure 2 + 2 is 5', async ({ assert }) => {
+  assert.equal(2 + 2, 5)
 })
