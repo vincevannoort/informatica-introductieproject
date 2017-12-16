81852ecccfb20ecfc653540402174de48016f010
Vince van Noort <vincevannoort@gmail.com>
2017-12-16 23:49:50 +0100

    Started working on promotion website

--- applicatie/webpack.mix.js
+++ applicatie/webpack.mix.js
@@ -13,7 +13,9 @@ const mix = require('laravel-mix')
 
 mix
   .js('resources/assets/js/app.js', 'public/js')
-  .sass('resources/assets/scss/app.scss', 'public/css')
+  .sass('resources/assets/scss/single-page-application.scss', 'public/css')
+  .sass('resources/assets/scss/promotion.scss', 'public/css')
   .setPublicPath('public')
+  .copyDirectory('resources/assets/images', 'public/images')
   .browserSync('localhost:3333')
   .disableSuccessNotifications();
