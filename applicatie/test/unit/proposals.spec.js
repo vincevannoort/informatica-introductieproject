4ad93ed7ff7a902dd23c0af09417560fb089487c
Vince van Noort <vincevannoort@gmail.com>
2017-12-19 14:14:23 +0100

    Written specifications for proposals & relations

---
+++ applicatie/test/unit/proposals.spec.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const { test, before, beforeEach, after, afterEach } = use('Test/Suite')('Proposals')
+
+test('Get all proposals', async ({ assert }) => {})
+
+test('Add a new proposal', async ({ assert }) => {})
+
+test('Edit a existing proposal', async ({ assert }) => {})
+
+test('Validate a new proposal', async ({ assert }) => {})
+
+test('Validate a existing proposal', async ({ assert }) => {})
+
+test('Attach a contact to an proposal', async ({ assert }) => {})
+
+test('Attach multiple contacts to an proposal', async ({ assert }) => {})
+
+test('Calculate insight after adding a new contact to an proposal', async ({ assert }) => {})
+
+test('Calculate insight after changing contact information of an contact attached to an proposal', async ({ assert }) => {})
---
+++ applicatie/test/unit/relations.spec.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const { test, before, after } = use('Test/Suite')('Relations')
+const Database = use('Database')
+
+const Relation = use('App/Models/Relation')
+
+before(async () => {
+
+})
+
+after(async () => {
+
+})
+
+test('Get all relations', async ({ assert }) => {})
+
+test('Add a new relation', async ({ assert }) => {})
+
+test('Edit a existing relation', async ({ assert }) => {})
+
+test('Validate a new relation', async ({ assert }) => {})
+
+test('Validate a existing relation', async ({ assert }) => {})
+
+test('Attach a contact to an relation', async ({ assert }) => {})
+
+test('Add a new proposal', async ({ assert }) => {})
+
+test('Calculate total insight based on proposals', async ({ assert }) => {})
+
+test('Seperate relations in 3 groups', async ({ assert }) => {})
