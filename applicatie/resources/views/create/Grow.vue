0f21fe8be51fa8378070968bf7dcb44e2e53ecde
Vince van Noort <vincevannoort@gmail.com>
2018-01-16 12:37:36 +0100

    Added files for create views

---
+++ applicatie/resources/views/create/Grow.vue
@@ -0,0 +1,5 @@
+<template>
+  <modal-create-edit :entity="'competition'">
+    <field v-model="example.name" :name="'Name'" :size="'full'" :validation="'required'" />
+  </modal-create-edit>
+</template>
